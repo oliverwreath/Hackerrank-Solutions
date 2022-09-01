a51952ed6b9a9c516361929182f3eb9bbe8aeb3d
oliverwreath <skywalkerhunter@gmail.com>
2022-08-31 23:08:39 -0400

    Cleanup Code.

---
+++ languages/Java/src/main/java/com/hackerrank/languages/java/introduction/JavaIntToString.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.hackerrank.languages.java.introduction;
+
+/**
+ * @author Kanahaiya Gupta
+ *
+ */
+public class JavaIntToString {
+	int n; //already decalared
+	String s=n+"";
+}
